f5308d9fe72d5f33cb9f111bab823bfa35bda26a
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 09:30:15 -0500

    commented out everything spotipy

--- final-project/main.py
+++ final-project/main.py
@@ -21,9 +21,9 @@ import webapp2
 import urllib2
 import json
 import urllib
-import spotipy
-import sys
-import spotipy.util as util
+# import spotipy
+# import sys
+# import spotipy.util as util
 
 # sp = spotipy.Spotify()
 
@@ -35,27 +35,27 @@ class MainHandler(webapp2.RequestHandler):
         my_template = jinja_environment.get_template("templates/test.html")
         self.response.write(my_template.render())
 
-class LoginHandler(webabb2.RequestHandler):
-    def get(self):
-        scope = 'user-library-read'
-
-        if len(sys.argv) > 1:
-            username = sys.argv[1]
-        else:
-            print "Usage: %s username" % (sys.argv[0],)
-            sys.exit()
-
-        token = util.prompt_for_user_token(username, scope)
-
-        if token:
-            sp = spotipy.Spotify(auth=token)
-            results = sp.current_user_saved_tracks()
-            for item in results['items']:
-                track = item['track']
-                print track['name'] + ' - ' + track['artists'][0]['name']
-        else:
-            print "Can't get token for", username
-        self.response.write("this is loaded")
+# class LoginHandler(webabb2.RequestHandler):
+#     def get(self):
+#         scope = 'user-library-read'
+#
+#         if len(sys.argv) > 1:
+#             username = sys.argv[1]
+#         else:
+#             print "Usage: %s username" % (sys.argv[0],)
+#             sys.exit()
+#
+#         token = util.prompt_for_user_token(username, scope)
+#
+#         if token:
+#             sp = spotipy.Spotify(auth=token)
+#             results = sp.current_user_saved_tracks()
+#             for item in results['items']:
+#                 track = item['track']
+#                 print track['name'] + ' - ' + track['artists'][0]['name']
+#         else:
+#             print "Can't get token for", username
+#         self.response.write("this is loaded")
 
 
 app = webapp2.WSGIApplication([
