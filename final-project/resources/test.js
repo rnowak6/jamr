320a3164e9bce5d1dffec34d87c991e8f634d3ea
CSS <css@CSSs-MacBook-Air.local>
2017-08-01 10:27:12 -0500

    maps

--- final-project/resources/test.js
+++ final-project/resources/test.js
@@ -3,7 +3,7 @@ var service;
 var infowindow;
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+  var pyrmont = new google.maps.LatLng(41.881832,	-87.623177);
 
   map = new google.maps.Map(document.getElementById('map'), {
       center: pyrmont,
