36b27133d40555fbd466bf386d35b5e01bc0f0bf
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 17:20:24 -0500

    deleted merge markers

--- final-project/main.py
+++ final-project/main.py
@@ -62,47 +62,8 @@ class LoginHandler(webapp2.RequestHandler):
         my_template=jinja_environment.get_template("templates/login.html")
         self.response.write(my_template.render())
 
-<<<<<<< HEAD
-# class AmeliaHandler(webapp2.RequestHandler):
-#     def get(self):
-#         scope = 'user-library-read'
-#
-#         if len(sys.argv) > 1:
-#             username = sys.argv[1]
-#         else:
-#             print "Usage: %s username" % (sys.argv[0],)
-#             sys.exit()
-#
-#         token = util.prompt_for_user_token(username, scope)
-#
-#         if token:
-#             sp = spotipy.Spotify(auth=token)
-#             results = sp.current_user_saved_tracks()
-#             for item in results['items']:
-#                 track = item['track']
-#                 print track['name'] + ' - ' + track['artists'][0]['name']
-#         else:
-#             print "Can't get token for", username
-# <<<<<<< HEAD
-# =======
-# #         self.response.write("this is loaded")
-# >>>>>>> 66c3a0e7ff3033229f05b8c89793785ae9920ccc
-#
-#
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-# <<<<<<< HEAD
-#     # ('/Amelia', AmeliaHandler)
-# =======
-    # ('/login',LoginHandler)
-# >>>>>>> 66c3a0e7ff3033229f05b8c89793785ae9920ccc
-=======
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-<<<<<<< HEAD
     ('/login',LoginHandler)
-=======
->>>>>>> 309431d13af94d166777212558fb51cf3148db83
->>>>>>> d043b2b2226c2cd55eeba4307815e92a984ae485
 ], debug=True)
