a5e73c5882ef59c5bbd52e93fd64076e510da742
CSSI <css@CSSs-MacBook-Air.local>
2017-08-03 09:41:40 -0500

    specific genres

--- final-project/getTheLocation.py
+++ final-project/getTheLocation.py
@@ -25,25 +25,25 @@ def playlistGenre(getGenres):
         "alternative" : 0
     }
     for item in getGenres:
-        if item == "rock":
+        if item == "rock" or item == "classic rock" or item == "hard rock" or item == "garage rock":
             genreDict["rock"] = genreDict["rock"] + 1
-        if item == "pop":
+        if item == "pop" or item == "pop rap" or item == "post-teen pop" or item == "pop rock" or item == "viral pop":
             genreDict['pop'] = genreDict['pop'] + 1
-        if item == "metal":
+        if item == "metal" or item == "alternative metal" or item == "nu metal" or item == "rap metal":
             genreDict["metal"] = genreDict["metal"] + 1
-        if item == "blues":
+        if item == "blues" or item == "soul" or item == "blues rock":
             genreDict["blues"] = genreDict["blues"] + 1
         if item == "dance pop":
             genreDict["dancepop"] = genreDict["dancepop"] + 1
-        if item == "rap":
+        if item == "rap" or item == "urban contemporary" or item == "dirty south rap" or item == "gangster rap":
             genreDict["rap"] = genreDict["rap"] + 1
-        if item == "tropical house":
+        if item == "tropical house" or item =="neo mellow" or item == "mello gold" or item == "indietronica" or item == "synthpop" or item == "tropical":
             genreDict["tropicalHouse"] = genreDict["tropicalHouse"] + 1
-        if item == "trap music":
+        if item == "trap music" or item == "dwn trap" or item == "trap latino":
             genreDict["trapMusic"] = genreDict["trapMusic"] + 1
-        if item == "modern rock":
+        if item == "modern rock" or item == "":
             genreDict["modernRock"] = genreDict["modernRock"] + 1
-        if item == "hip hop":
+        if item == "hip hop" or item == "latin hip hop":
             genreDict["hipHop"] = genreDict["hipHop"] + 1
         if item == "classical":
             genreDict["classical"] = genreDict["classical"] + 1
