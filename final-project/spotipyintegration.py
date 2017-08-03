068d04ab9047a5d0adc18582492a9ae578b2b390
CSSI <css@CSSs-MacBook-Air.local>
2017-08-03 10:16:37 -0500

    saddsaasdasd

--- final-project/spotipyintegration.py
+++ final-project/spotipyintegration.py
@@ -12,11 +12,8 @@ import json
 #my_user=spotifyUserInfo(postUserName=amelialutz9)
 #print client_credentials_manager
 
-<<<<<<< HEAD
-user=raw_input("Username: ")
-=======
 # user=raw_input("Username: ")
->>>>>>> c75d5aa9cc6054868d8c3c28a7cadc46a5467c37
+# user=raw_input("Username: ")
 def getGenres(user):
     client_credentials_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -48,9 +45,6 @@ def getGenres(user):
         playlist_tracks = None
     return types_of_songs#
 
-<<<<<<< HEAD
 print getGenres(user)
-=======
 # user=raw_input("Username: ")
 # print getGenres(user)
->>>>>>> c75d5aa9cc6054868d8c3c28a7cadc46a5467c37
